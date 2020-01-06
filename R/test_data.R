7b5c7be22aaddd98a8c65b8b82a74320d8d871dd
jennifervk <55493049+jennifervk@users.noreply.github.com>
2020-01-06 15:30:34 +0100

    Add files via upload

---
+++ R/test_data.R
@@ -0,0 +1,23 @@
+#' Formal class RasterBrick
+#' @name stN
+#' @description 5x5 RasterBrick containing 476 Landsta scenes between 1984-2011. Includes NA values. Located in the Randi forest area (southern Cyprus).
+#' @docType data
+#' @usage data(stN)
+#' @author NASA/ESA/Jennifer von Keyserlingk
+#' @keywords data
+#' @examples
+#' data(stN)
+#' plot(stN, 1:9)
+
+NULL
+
+#' Date vector of Landsat scenes included in stN
+#' @name d
+#' @description Vector containing dates of all Landsat scenes included in stN
+#' @docType data
+#' @usage data(d)
+#' @author Jennifer von Keyserlingk
+#' @keywords data
+
+NULL
+
